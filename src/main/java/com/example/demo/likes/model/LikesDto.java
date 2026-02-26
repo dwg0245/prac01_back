8d73b9b707aefac08308bc12162add179274dd64
dwg245 <dwg0245@gmail.com>
2026-02-26 11:16:40 +0900

    [feat/jpa/relation] 좋아요 추가 기능 구현 - 게시글 idx와 사용자 idx를 받아 저장 및 응답

--- src/main/java/com/example/demo/likes/model/LikesDto.java
+++ src/main/java/com/example/demo/likes/model/LikesDto.java
@@ -1,10 +1,46 @@
 package com.example.demo.likes.model;
 
+import com.example.demo.board.model.Board;
+import com.example.demo.user.model.User;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Builder
+
 public class LikesDto {
-    private Long idx;
+    @Getter
+    @Builder
+    public static class RegReq{
+        private Long idx;
+
+        public Likes toEntity(Long boardIdx, Long userIdx){
+            return Likes.builder()
+                    .idx(this.idx)
+                    .board(Board.builder()
+                            .idx(boardIdx)
+                            .build())
+                    .user(User.builder()
+                            .idx(userIdx)
+                            .build())
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    public static class RegRes{
+        private Long idx;
+        private Long boardIdx;
+        private Long userIdx;
+
+        public static LikesDto.RegRes from(Likes entity){
+            return RegRes.builder()
+                    .idx(entity.getIdx())
+                    .boardIdx(entity.getBoard().getIdx())
+                    .userIdx(entity.getUser().getIdx())
+                    .build();
+        }
+    }
+
 }
