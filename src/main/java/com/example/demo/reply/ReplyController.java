e485a5825bb23eac61d0b50a9e6635e8ea8345bd
dwg245 <dwg0245@gmail.com>
2026-02-26 09:49:30 +0900

    [feat/security/get_user_info] 현재 로그인 한 사용자 정보 받아오기 JwtFilter에서 AuthUserDetails에 사용자 정보를 담아주면
    컨트롤러의 매개변수에서 @AuthenticationPrincipal AuthUserDetails user 코드로 사용자 객체를 받아올 수 있다.

--- src/main/java/com/example/demo/reply/ReplyController.java
+++ src/main/java/com/example/demo/reply/ReplyController.java
@@ -2,11 +2,11 @@ package com.example.demo.reply;
 
 import com.example.demo.common.model.BaseResponse;
 import com.example.demo.reply.model.ReplyDto;
+import com.example.demo.user.model.AuthUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/reply")
 @RequiredArgsConstructor
@@ -14,10 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReplyController {
     private final ReplyService replyService;
 
-    @RequestMapping("/read/{idx}")
+    @GetMapping("/read/{idx}")
     public ResponseEntity read(@PathVariable Long idx){
         ReplyDto.Replyres result = replyService.read(idx);
         return ResponseEntity.ok(BaseResponse.success(result));
+    }
+
+    @PostMapping("/reg/{boardIdx}")
+    public ResponseEntity reg(@AuthenticationPrincipal AuthUserDetails user, @PathVariable Long boardIdx){
+        return  ResponseEntity.ok(BaseResponse.success("성공"));
     }
 
 }
