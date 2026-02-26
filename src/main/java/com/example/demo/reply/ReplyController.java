0f644487279e187d005237626d5f07f060f62454
dwg245 <dwg0245@gmail.com>
2026-02-26 10:56:50 +0900

    [feat/jpa/relation] 댓글 작성 기능 구현 - 게시글 idx, 사용자 idx 포함 저장 및 응답 - 사용자 idx로 유저 정보 담아와서 해당 유저이름 응답

--- src/main/java/com/example/demo/reply/ReplyController.java
+++ src/main/java/com/example/demo/reply/ReplyController.java
@@ -4,9 +4,7 @@ import com.example.demo.common.model.BaseResponse;
 import com.example.demo.reply.model.ReplyDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/reply")
 @RequiredArgsConstructor
@@ -14,10 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReplyController {
     private final ReplyService replyService;
 
-    @RequestMapping("/read/{idx}")
+    @PostMapping("/reg/{boardIdx}/{userIdx}")
+    public ResponseEntity reg(@PathVariable Long boardIdx,@PathVariable Long userIdx,@RequestBody ReplyDto.RegReq dto){
+        ReplyDto.Replyres result = replyService.reg(boardIdx, userIdx, dto);
+
+        return ResponseEntity.ok(BaseResponse.success(result));
+    };
+
+
+    @GetMapping("/read/{idx}")
     public ResponseEntity read(@PathVariable Long idx){
         ReplyDto.Replyres result = replyService.read(idx);
         return ResponseEntity.ok(BaseResponse.success(result));
     }
-
 }
